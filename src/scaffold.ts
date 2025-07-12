5045f60b6d5ad7d1363a477b7169f72e27cad6db
Jude.K <kiggundujude120@gmail.com>
2025-07-12 08:46:49 +0300

    Added console log for port listening

--- src/scaffold.ts
+++ src/scaffold.ts
@@ -96,7 +96,7 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
- console.log('Server running on port \${port}\')})
+ console.log(\`Server running on port \${port}\`)})
 `);
 
   console.log(`
