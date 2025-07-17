80d09053408696f749260565271a1d522c5ffd2e
Jude.K <132937180+judekigsepc@users.noreply.github.com>
2025-07-17 15:23:00 +0300

    Update schemaRegistry.ts

--- templates/utils/validation/schemaRegistry.ts
+++ templates/utils/validation/schemaRegistry.ts
@@ -1,11 +1,8 @@
 import { userLoginSchema, userSchema } from "../../modules/auth/user.schema";
-import { addGroupMembersSchema, groupSchema } from "../../modules/groups/group.schemas";
 
 export const validationSchemaMap = {
   "user" : userSchema,
   "user-login": userLoginSchema,
-  "group": groupSchema,
-  "add-group-members": addGroupMembersSchema
 };
 
 export type ValidationTypes = keyof typeof validationSchemaMap;
